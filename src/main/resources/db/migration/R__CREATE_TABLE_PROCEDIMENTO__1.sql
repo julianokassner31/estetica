200df070879d85ce22347d56ef9d05db13e56d49
Juliano Kassner <julianokassner@hotmail.com>
2025-08-15 23:00:02 -0300

    Create table procedimento_material

--- src/main/resources/db/migration/R__CREATE_TABLE_PROCEDIMENTO__1.sql
+++ src/main/resources/db/migration/R__CREATE_TABLE_PROCEDIMENTO__1.sql
@@ -1,12 +1,14 @@
-CREATE TABLE procedimento (
+CREATE TABLE IF NOT EXISTS procedimento (
      id serial PRIMARY KEY,
      nome varchar(50),
      valor numeric(8,2),
      descricao text
 );
 
-CREATE TABLE procedimento_material (
+CREATE TABLE IF NOT EXISTS procedimento_material (
      id serial PRIMARY KEY,
      id_procedimento int not null,
      id_material int not null
 );
+
+ALTER TABLE procedimento_material ADD COLUMN qtd_material numeric(8,2) not null;

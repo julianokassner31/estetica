462f43fbd8111e9271102f1cad4285bbf9aa5b3f
Juliano Kassner <julianokassner@hotmail.com>
2025-12-17 00:09:30 -0300

    Separando configs

--- src/main/java/br/com/jkassner/estetica/EsteticaApplication.java
+++ src/main/java/br/com/jkassner/estetica/EsteticaApplication.java
@@ -12,15 +12,4 @@ public class EsteticaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EsteticaApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
-
 }

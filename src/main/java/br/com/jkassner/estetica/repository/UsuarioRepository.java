b329cbf8db897aa7f5caac44ccf2b2b7e1eef93a
Juliano Kassner <julianokassner@hotmail.com>
2026-01-18 18:38:42 -0300

    Ajuste classe

--- src/main/java/br/com/jkassner/estetica/repository/UsuarioRepository.java
+++ src/main/java/br/com/jkassner/estetica/repository/UsuarioRepository.java
@@ -1,8 +1,6 @@
 package br.com.jkassner.estetica.repository;
 
 import br.com.jkassner.estetica.model.Usuario;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;

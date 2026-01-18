7a10e6b0d28ec49b1e34bc9a174a2aa61b6942d9
Juliano Kassner <julianokassner@hotmail.com>
2026-01-18 18:37:35 -0300

    Acessar enpoint com authorithies

--- src/main/java/br/com/jkassner/estetica/controller/UsuarioController.java
+++ src/main/java/br/com/jkassner/estetica/controller/UsuarioController.java
@@ -4,12 +4,13 @@ import br.com.jkassner.estetica.dtos.UsuarioDto;
 import br.com.jkassner.estetica.dtos.PaginationDto;
 import br.com.jkassner.estetica.mapper.UsuarioMapper;
 import br.com.jkassner.estetica.model.Usuario;
-import br.com.jkassner.estetica.repository.UsuarioRepository;
+import br.com.jkassner.estetica.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,46 +22,50 @@ import java.util.Optional;
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepository repository;
+    private UsuarioService usuarioService;
 
-    @GetMapping
-    /**public ResponseEntity<PaginationDto<UsuarioDto>> getUsuarios(@RequestParam boolean func, @RequestParam int pageIndex, @RequestParam int pageSize) {
-        PageRequest pageRequest = PageRequest.of(pageIndex, pageSize, Sort.by(Sort.Direction.ASC, "nome"));
-        Page<Usuario> pagination = this.repository.findByFunc(func, pageRequest);
-        List<UsuarioDto> pacientesDto = new ArrayList<>(0);
-        pagination.getContent().forEach(paciente ->  {
-            pacientesDto.add(UsuarioMapper.INSTANCE.modelToDto(paciente));
+    @PreAuthorize("hasAnyAuthority('CADASTRO', 'ATENDIMENTO', 'LEITURA')")
+    @GetMapping(value = "/func")
+    public ResponseEntity<PaginationDto<UsuarioDto>> getUsuariosFunc(@RequestParam int pageIndex, @RequestParam int pageSize) {
+            PageRequest pageRequest = PageRequest.of(pageIndex, pageSize, Sort.by(Sort.Direction.ASC, "nome"));
+        Page<Usuario> pagination = this.usuarioService.findAll(pageRequest);
+        List<UsuarioDto> usuarioDtos = new ArrayList<>(0);
+        pagination.getContent().forEach(usuario ->  {
+            usuarioDtos.add(UsuarioMapper.INSTANCE.modelToDto(usuario));
         });
 
         PaginationDto<UsuarioDto> materialDtoPaginationDto = new PaginationDto<>();
-        materialDtoPaginationDto.setElements(pacientesDto);
+        materialDtoPaginationDto.setElements(usuarioDtos);
         materialDtoPaginationDto.setPage(pageIndex);
         materialDtoPaginationDto.setSize(pagination.getSize());
         materialDtoPaginationDto.setTotalPages(pagination.getTotalPages());
         materialDtoPaginationDto.setTotalElements(pagination.getTotalElements());
 
         return ResponseEntity.ok(materialDtoPaginationDto);
-    }*/
+    }
 
+    @PreAuthorize("hasAnyAuthority('CADASTRO', 'CLIENTE')")
     @PostMapping(value = "/cliente")
-    public ResponseEntity<UsuarioDto> create(@RequestBody UsuarioDto req) {
+    public ResponseEntity<UsuarioDto> createCliente(@RequestBody UsuarioDto req) {
         Usuario usuario = UsuarioMapper.INSTANCE.dtoToModel(req);
-        usuario = repository.save(usuario);
+        usuario = usuarioService.save(usuario);
 
         return ResponseEntity.ok(UsuarioMapper.INSTANCE.modelToDto(usuario));
     }
 
+    @PreAuthorize("hasAnyAuthority('CADASTRO')")
     @PostMapping(value = "/func")
     public ResponseEntity<UsuarioDto> createFunc(@RequestBody UsuarioDto req) {
        Usuario usuario = UsuarioMapper.INSTANCE.dtoToModel(req);
-        usuario = repository.save(usuario);
+        usuario = usuarioService.save(usuario);
 
         return ResponseEntity.ok(UsuarioMapper.INSTANCE.modelToDto(usuario));
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR', 'CADASTRO', 'LEITURA')")
     @GetMapping(value = "/findByNome")
     public ResponseEntity<List<UsuarioDto>> findByName(@RequestParam String nome) {
-        List<Usuario> usuarios = repository.findByNomeContainsIgnoreCase(nome);
+        List<Usuario> usuarios = usuarioService.findByNomeContainsIgnoreCase(nome);
 
         if (usuarios.isEmpty()) return ResponseEntity.noContent().build();
 
@@ -72,9 +77,10 @@ public class UsuarioController {
         return ResponseEntity.ok(usuarioDtos);
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR', 'CADASTRO', 'LEITURA')")
     @GetMapping(value = "/findById/{id}")
     public ResponseEntity<UsuarioDto> findByName(@PathVariable Integer id) {
-        Optional<Usuario> usuario = repository.findById(id);
+        Optional<Usuario> usuario = usuarioService.findById(id);
         return usuario.map(value -> ResponseEntity.ok(UsuarioMapper.INSTANCE.modelToDto(value))).orElseGet(() -> ResponseEntity.noContent().build());
     }
 }

d25cb63934727ecdfe3b49e9f8eb9d811a07a882
Juliano Kassner <julianokassner@hotmail.com>
2026-01-21 23:00:04 -0300

    Habilitando audit jpa

---
+++ src/main/java/br/com/jkassner/estetica/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package br.com.jkassner.estetica.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
